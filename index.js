4a01eaaeafd89c03e6c354e7b9bac7a94ecc6a56
Ben Johnson <benjoh@amazon.com>
2018-03-10 08:47:07 -0800

    Added hardcoded response for votd

--- index.js
+++ index.js
@@ -5,7 +5,21 @@ exports.handler = function(event, context, callback) {
   console.log('event', event);
   console.log('==================================');
   console.log('Stopping index.handler');
-  callback(null, event);
+
+  // hardcoded response for now
+  var responseBody = {
+    book: "John",
+    chapter: "12",
+    verse_start: "27",
+    verse_end: "28"
+  };
+
+  var response = {
+    statusCode: "200",
+    body: JSON.stringify(responseBody)
+  };
+
+  callback(null, response);
   // or
   // callback( 'some error type' );
 };
