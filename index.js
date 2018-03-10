a5f28163848dcf787d9e292c16888890420551d1
Ben Johnson <benjoh@amazon.com>
2018-03-10 11:48:11 -0800

    Added support for V1 of getScripture API

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 "use strict";
 
 var _ = require('lodash')
+, request = require('request')
 , votd = require('./data/votd.js')
 , ntBooks = require('./data/ntBooks.js')
 , languageVolumeMap = require('./data/languageVolumeMap.js')
+, dbpApiKey = process.env.DBP_API_KEY;
 
 exports.handler = function(event, context, callback) {
   console.log('Beginning Execution');
@@ -38,7 +40,6 @@ exports.handler = function(event, context, callback) {
 }
 
 var getVerseOfTheDay = function(event, context, callback) {
-
   // default verse
   var reference = {
     book: "John",
@@ -63,12 +64,86 @@ var getVerseOfTheDay = function(event, context, callback) {
   callback(null, response);
 };
 
+var getLanguage = function(event, context){
+  // first check the query string params:
+  let language = event["queryStringParameters"]["language"];
+  // if nothing there, then check the body
+  if (_.isEmpty(language)) {
+    language = event["body"]["language"];
+  }
+  // last resort use default
+  if (_.isEmpty(language)) {
+    language = "en";
+  }
+  return language;
+};
+
 var getScripture = function(event, context, callback) {
-  var response = {
-    statusCode: 500,
-    body: JSON.stringify({response:"Unsupported Operation"})
-  };
-  callback(null, response);
+  // if empty body, just pick a random verse
+  var reference = event["body"];
+  if (_.isEmpty(event["body"])) {
+    reference = votd[_.random(votd.length-1)];
+  }
+  console.log("reference = " + reference);
+  var language = reference.language || 'en';
+  var dam_id = getVolume(reference.book, language);
+  var dbtUrl = 'http://dbt.io/text/verse?v=2&key=' +
+    dbpApiKey +
+    '&book_id=' + reference.book +
+    '&chapter_id=' + reference.chapter +
+    '&verse_start=' + reference.verse_start +
+    '&verse_end=' + reference.verse_end +
+    '&dam_id=' + dam_id;
+  console.log('Getting verse from ' + dbtUrl);
+
+  request.get({url:dbtUrl, json: true}, function (e, r, verses) {
+    if (e) {
+      res.send(r.statusCode, e);
+      callback(null, {statusCode: r.statusCode, body: e });
+    } else {
+      callback(null, formatScripture(verses, language));
+    }
+  });
+};
+
+var formatScripture = function (verses, language) {
+  if (_.isEmpty(verses)) {
+    console.error('Failed to get verses.');
+    return {
+      text: 'Pray without ceasing.',
+      citation: '1 Thessalonians 5:17',
+      bible: 'ENGESV'
+    };
+  } else {
+    var citation = verses[0].book_name + ' ' +
+      verses[0].chapter_id + ':' + verses[0].verse_id;
+
+    if (_.size(verses) > 1) {
+      citation += '-' + _.last(verses).verse_id;
+    }
+    citation += ' ' + getVolumeCode(language);
+
+    var details = languageVolumeMap[language] || languageVolumeMap['default'];
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        text: _.map(verses, 'verse_text')
+          .map(function (v) { return v.trim(); }).join(' '),
+        citation: citation,
+        bible: details.bible || 'ENGESV'
+      })
+    };
+  }
+};
+
+var getVolume = function (book, language) {
+  var details = languageVolumeMap[language] || languageVolumeMap['default'];
+  return ntBooks.indexOf(book) > -1 ? details.nt : details.ot;
+};
+
+var getVolumeCode = function (language) {
+  var details = languageVolumeMap[language] || languageVolumeMap['default'];
+  return details.code;
 };
 
 var getAScriptureImage = function(event, context, callback) {
