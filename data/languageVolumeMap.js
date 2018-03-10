01b0c18262882f004c19c34fe5b7234d2d4dbd30
Ben Johnson <benjoh@amazon.com>
2018-03-10 11:09:54 -0800

    Copied and refactored VOTD code from ceaseless-lite

---
+++ data/languageVolumeMap.js
@@ -0,0 +1,44 @@
+module.exports = {
+  'zh_CN': {
+    code: 'UN1',
+    nt: 'CHNUN1N2ET',
+    ot: 'CHNUN1O2ET',
+    bible: 'CHNUN1'
+  },
+  'in_ID': {
+    code: 'NTV',
+    nt: 'INZNTVN2ET',
+    ot: 'INZNTVO2ET',
+    bible: 'INZNTV'
+  },
+  'hi_IN': {
+    code: 'WTC',
+    nt: 'HNDWTC',
+    ot: 'HNDWTC',
+    bible: 'HNDWTC'
+  },
+  'ko_KR': {
+    code: 'SKV',
+    nt: 'KO1SKVN2ET',
+    ot: 'KO1SKVO2ET',
+    bible: 'KO1SKV'
+  },
+  'es': {
+    code: 'BDA',
+    nt: 'SPNBDAN2ET',
+    ot: 'SPNBDAO2ET',
+    bible: 'SPNBDA'
+  },
+  'jp': {
+    code: 'CJV',
+    nt: 'JPNCJVN2ET',
+    ot: 'JPNCJVO2ET',
+    bible: 'JPNCJV'
+  },
+  'default': {
+    code: 'ESV',
+    nt: 'ENGESVN2ET',
+    ot: 'ENGESVO2ET',
+    bible: 'ENGESV'
+  }
+};
--- index.js
+++ index.js
@@ -1,25 +1,80 @@
-// For development/testing purposes
+"use strict";
+
+var _ = require('lodash')
+, votd = require('./data/votd.js')
+, ntBooks = require('./data/ntBooks.js')
+, languageVolumeMap = require('./data/languageVolumeMap.js')
+
 exports.handler = function(event, context, callback) {
-  console.log('Running index.handler');
+  console.log('Beginning Execution');
   console.log('==================================');
   console.log('event', event);
   console.log('==================================');
-  console.log('Stopping index.handler');
 
-  // hardcoded response for now
-  var responseBody = {
+  // simple request handler for different APIs
+  let handlers = {
+    "/votd": getVerseOfTheDay,
+    "/getScripture": getScripture,
+    "/getAScriptureImage": getAScriptureImage,
+
+  };
+  let resource = event["resource"];
+  if (resource in handlers) {
+    return handlers[resource](event, context, callback);
+  }
+
+  const response = {
+    statusCode: 405,
+    headers: {
+      "Access-Control-Allow-Origin" : "*",
+      "Access-Control-Allow-Credentials" : true
+    },
+    body: JSON.stringify({
+      message: `Invalid HTTP Resource: ${resource}`
+    }),
+  };
+
+  callback(null, response);
+}
+
+var getVerseOfTheDay = function(event, context, callback) {
+
+  // default verse
+  var reference = {
     book: "John",
     chapter: "12",
     verse_start: "27",
     verse_end: "28"
   };
+  let language = event["queryStringParameters"]["language"];
+  if (language == 'hi_IN') {
+    do {
+      reference = votd[_.random(votd.length-1)];
+    } while (ntBooks.indexOf(reference.book) <= -1);
+  } else {
+    reference = votd[_.random(votd.length-1)];
+  }
 
   var response = {
-    statusCode: "200",
-    body: JSON.stringify(responseBody)
+    statusCode: 200,
+    body: JSON.stringify(reference)
   };
 
   callback(null, response);
-  // or
-  // callback( 'some error type' );
+};
+
+var getScripture = function(event, context, callback) {
+  var response = {
+    statusCode: 500,
+    body: JSON.stringify({response:"Unsupported Operation"})
+  };
+  callback(null, response);
+};
+
+var getAScriptureImage = function(event, context, callback) {
+  var response = {
+    statusCode: 500,
+    body: JSON.stringify({response:"Unsupported Operation"})
+  };
+  callback(null, response);
 };
